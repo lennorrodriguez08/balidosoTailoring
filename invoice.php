8e44af6fa9f805e338fd6e8487322c6892bd7273
Roy <royallenmoreno@gmail.com>
2020-04-21 18:39:24 +0800

    21/04/2020 6:39pm

--- invoice.php
+++ invoice.php
@@ -1,16 +1,12 @@
-<?php
 
-echo $_GET['name']."\r\n". 
-$_GET['transactionId']."\r\n".
-$_GET['date']."\r\n".
-$_GET['items']."\r\n".
-$_GET['qty']."\r\n".
-$_GET['presc']."\r\n".
-$_GET['totalPrc']."\r\n".
-$_GET['rcv']."\r\n".
-$_GET['bal']."\r\n".
-$_GET['contact']."\r\n".
-$_GET['local'];
-
-
-?>
+<strong>Name: </strong> <?php echo $_GET['name'] ?> <br>
+<strong>Transaction id: </strong> <?php echo $_GET['transactionId'] ?> <br>
+<strong>date: </strong> <?php echo $_GET['date'] ?> <br>
+<strong>items: </strong> <?php echo $_GET['items'] ?> <br>
+<strong>quantity: </strong> <?php echo $_GET['qty'] ?> <br>
+<strong>prescription: </strong> <?php echo $_GET['presc'] ?> <br>
+<strong>totalprice: </strong> <?php echo $_GET['totalPrc'] ?> <br>
+<strong>receive: </strong> <?php echo $_GET['rcv'] ?> <br>
+<strong>balance: </strong> <?php echo $_GET['bal'] ?> <br>
+<strong>contact: </strong> <?php echo $_GET['contact'] ?> <br>
+<strong>local: </strong> <?php echo $_GET['local'] ?> <br>
