c53dd22d8c621abb6c1dac446ad59d392bc15b41
Roy <royallenmoreno@gmail.com>
2020-04-21 18:34:10 +0800

    21/04/2020 6:34pm

--- invoice.php
+++ invoice.php
@@ -1,13 +1,16 @@
 <?php
 
 echo $_GET['name']."\r\n". 
+$_GET['transactionId']."\r\n".
 $_GET['date']."\r\n".
 $_GET['items']."\r\n".
 $_GET['qty']."\r\n".
 $_GET['presc']."\r\n".
 $_GET['totalPrc']."\r\n".
 $_GET['rcv']."\r\n".
-$_GET['bal'];
+$_GET['bal']."\r\n".
+$_GET['contact']."\r\n".
+$_GET['local'];
 
 
 ?>
